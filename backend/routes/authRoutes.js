f65a6ed7a5182fedc23ca8c4aa27b0270110ad11
Om Nepal <nepalom15@gmail.com>
2025-07-07 12:11:54 -0500

    add auth Middleware that protects some routes from unauthenticated users by verifying req's jwt

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const {registerUser, loginnUser, getUserProfile} = require("../controllers/authController");
-const {protect} = require("../middleware/authMiddleware");
+const {registerUser, loginUser, getUserProfile} = require("../controllers/authController");
+const {protect} = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
