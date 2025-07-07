600b3cb87343bf400d35987ea9cd43983d8b968b
Om Nepal <nepalom15@gmail.com>
2025-07-06 22:34:19 -0500

    start backend code with basic server.js structure and necessary imports

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Middleware to handle CORS
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+//Middleware to parse JSON bodies
+app.use(express.json());
+
+//Routes
+
+
+//Server uploads foler
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}))
+
+//Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
