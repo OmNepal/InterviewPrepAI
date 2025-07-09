0bab486383bbe9dccf904281ee09ab93c321cbfc
Om Nepal <nepalom15@gmail.com>
2025-07-09 18:02:06 -0500

    add methods in questionController to add questions, pin/unpin a question and update a question's note

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -4,9 +4,34 @@ const Session = require("../models/Session");
 // @desc    Add questions to a session
 // @route   POST /api/questions/add
 // @access  Private(requires JWT)
-const addQuestionsToSession = async(req,res) => {
+exports.addQuestionsToSession = async(req,res) => {
     try {
+        const {sessionId, questions} = req.body;
 
+        if (!sessionId || !questions || !Array.isArray(questions)) {
+            return res.status(400).json({message: "Invalid Input Data"})
+        }
+
+        const session = await Session.findById(sessionId);
+
+        if (!session) {
+            return res.status(404).json({message: "Session not found"})
+        }
+
+        //Create new questions
+        const createdQuestions = await Question.insertMany(
+            questions.map((q) => ({
+                session: sessionId,
+                question: q.question,
+                answer: q.anser
+            }))
+        );
+
+        //Update session to include new question IDs
+        session.questions.push(createdQuestions.map((q) => q._id))
+        await session.save();
+
+        res.status(201).json({createdQuestions});
     } catch (error) {
         res.status(500).json({message: "Server error", error: error.message})
     }
@@ -15,8 +40,18 @@ const addQuestionsToSession = async(req,res) => {
 // @desc    Pin/Unpin a question
 // @route   POST /api/questions/:id/pin
 // @access  Private(requires JWT)
-const togglePinQuestion = async(req,res) => {
+exports.togglePinQuestion = async(req,res) => {
     try {
+        const question = await Question.findById(req.params.id);
+
+        if (!question) {
+            res.status(404).json({success: false, message: "Question not found"})
+        }
+
+        question.isPinned = !question.isPinned;
+        await question.save();
+
+        res.status(200).json({success: true, question})
 
     } catch (error) {
         res.status(500).json({message: "Server error", error: error.message})
@@ -26,9 +61,19 @@ const togglePinQuestion = async(req,res) => {
 // @desc    Update the notes of a questions
 // @route   POST /api/questions/:id/note
 // @access  Private(requires JWT)
-const updateQuestionsNote = async(req,res) => {
+exports.updateQuestionNote = async(req,res) => {
     try {
+        const { note } = req.body;
+        const question = await Question.findById(req.params.id);
 
+        if (!question) {
+            res.status(404).json({success: false, message: "Question not found"})
+        }
+
+        question.note = note || "";
+        await question.save();
+
+        res.status(200).json({success: true, message: "Note updated successfully"})
     } catch (error) {
         res.status(500).json({message: "Server error", error: error.message})
     }
--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const {togglePinQuestion, updateQuestionsNote, addQuestionsToSession} = require("../controllers/questionController");
+const {togglePinQuestion, updateQuestionNote, addQuestionsToSession} = require("../controllers/questionController");
 const {protect} = require("../middlewares/authMiddleware")
 
 const router = express.Router();
 
 router.post("/add",protect, addQuestions);
 router.post("/:id/pin", protect, togglePinQuestion);
-router.post("/:id/note", protect, updateQuestionsNote);
+router.post("/:id/note", protect, updateQuestionNote);
 
 module.exports = router;
