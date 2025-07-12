bd20594635f5fae20554a23d107695f7f6198d6d
Om Nepal <nepalom15@gmail.com>
2025-07-11 20:52:35 -0500

    add login call to backend so user can log in, add userContext so the entire app can get user data and some additional utility functions

--- frontend/interview-prep-ai/src/pages/Auth/Login.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/Login.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input"
 import {validateEmail} from "../../utils/helper"
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import { UserContext } from "../../context/userContext";
 
 const Login = ({ setCurrentPage }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+
+  const {updateUser} = useContext(UserContext);
 
   const navigate = useNavigate();
 
@@ -32,6 +37,18 @@ const Login = ({ setCurrentPage }) => {
 
     //Login API call
     try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password
+      })
+
+      const {token} = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(response.data);
+        navigate("/dashboard");
+      }
 
     } catch (e) {
       if (e.response || e.response.data.message) {
